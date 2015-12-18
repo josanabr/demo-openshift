442d1220b0133cdf0aa3383cc29da38c18d6e1df
John Sanabria <john.sanabria@gmail.com>
2015-12-18 05:37:22 +0000

    fixing typo

--- wsgi/routes.py
+++ wsgi/routes.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 @app.route("/")
 def insult():
-	conn = pymysql.connection(host = os.environ['OPENSHIFT_MYSQL_DB_HOST'], port=os.environ['OPENSHIFT_MYSQL_DB_PORT'], user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'], passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'], db=os.environ['OPENSHIFT_MYSQL_DB_URL'])
+	conn = pymysql.connect(host = os.environ['OPENSHIFT_MYSQL_DB_HOST'], port=os.environ['OPENSHIFT_MYSQL_DB_PORT'], user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'], passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'], db=os.environ['OPENSHIFT_MYSQL_DB_URL'])
 	cur = conn.cursor()
 	cur.execute("Select * from events")
 	#return "Hello, code monkey!"
