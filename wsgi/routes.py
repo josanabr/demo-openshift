6e0b0e8ee74d5072eb1994c59f0a73f2dcc2f7d9
John Sanabria <john.sanabria@gmail.com>
2015-12-18 05:33:36 +0000

    Adding mysql instructions

--- wsgi/routes.py
+++ wsgi/routes.py
@@ -1,10 +1,15 @@
 import os
+import pymysql
 from flask import Flask
 app = Flask(__name__)
 
 app.config['PROPAGATE_EXCEPTIONS'] = True
 @app.route("/")
 def insult():
-	return "Hello, code monkey!"
+	conn = pymysql.connection(host = os.environ['OPENSHIFT_MYSQL_DB_HOST'], port=os.environ['OPENSHIFT_MYSQL_DB_PORT'], user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'], passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'], db=os.environ['OPENSHIFT_MYSQL_DB_URL'])
+	cur = conn.cursor()
+	cur.execute("Select * from events")
+	#return "Hello, code monkey!"
+	return cur.description
 if __name__ == "__main__":
 	app.run()
